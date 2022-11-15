410b944f5852c82780208d7e6f058942facd399c
rithiknagar <117367951+rithiknagar@users.noreply.github.com>
2022-11-15 12:37:07 +0530

    Add files via upload

---
+++ check whether its alphabet ,digit or special character.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+    {
+      char ch;
+      printf("enter the alphabet ");
+      scanf("%c",&ch);
+      if(ch>'A'&&ch<'Z')
+      printf(" alphabet upper case");
+      else if (ch>'a'&&ch<'z')
+      printf("alphabet lower case");
+      else if(ch>'0'&&ch<'9')
+      printf("digit");
+      else
+      printf("special character");
+       
+       return 0;
+      
+    }
