f1c20067e928c0efa15cb0dc38ab9f4b3f235e56
rithiknagar <117367951+rithiknagar@users.noreply.github.com>
2022-11-15 12:40:27 +0530

    Update check whether its alphabet ,digit or special character.c

--- check whether its alphabet ,digit or special character.c
+++ check whether its alphabet ,digit or special character.c
@@ -8,7 +8,7 @@ int main()
       printf(" alphabet upper case");
       else if (ch>'a'&&ch<'z')
       printf("alphabet lower case");
-      else if(ch>'0'&&ch<'9')
+      else if(ch>='0'&&ch<='9')
       printf("digit");
       else
       printf("special character");
